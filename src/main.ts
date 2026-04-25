aae3f472f976631f64f8c3df8d386d6f4cfc65ef
zhoucheng <zhoucheng133@outlook.com>
2026-04-25 21:00:21 +0800

    添加一些依赖

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,18 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+import 'vuetify/styles';
+import { createVuetify } from 'vuetify';
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
+import { createPinia } from "pinia";
 
-createApp(App).mount("#app");
+const pinia = createPinia();
+const vuetify = createVuetify({
+  components,
+  directives,
+})
+
+createApp(App)
+.use(vuetify)
+.use(pinia)
+.mount("#app");
