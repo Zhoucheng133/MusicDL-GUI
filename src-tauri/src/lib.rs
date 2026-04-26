2d683dd3dd6613a0d1d161f246aee63cb7496785
zhoucheng <zhoucheng133@outlook.com>
2026-04-26 21:29:48 +0800

    添加upload依赖

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -3,12 +3,13 @@ use std::path::Path;
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
 fn path_check(path: &str) -> bool {
-   return Path::new(&path).exists();
+    return Path::new(&path).exists();
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_upload::init())
         .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_http::init())
         .plugin(tauri_plugin_dialog::init())
